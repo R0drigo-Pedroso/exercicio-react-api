ab62fdfe197182c0d0d06f06cdae719b1e19cf90
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-11-01 11:59:56 -0300

    exercicio
    html estruturado.
    falta fazer o css.

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    async function getProdutos() {
+      try {
+        const resposta = await fetch("https://fakestoreapi.com/products");
+        const dados = await resposta.json();
+        setProdutos(dados);
+      } catch (error) {
+        console.log("Deu errado a aplicação " + error.message);
+      }
+    }
+    getProdutos();
+  }, []);
+
+  return (
+    <div>
+      {/* Função utilizando map */}
+      {produtos.map(({ id, title, price, image }) => {
+        return (
+          <article key={id}>
+            <h3>{title}</h3>
+            <p>{price}</p>
+            <img src={image} alt="" />
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
